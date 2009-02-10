8f0efbc7a63c74ea5d564778f26659bf7b46de94
Johan Andersson <johan.rejeep@gmail.com>
2009-02-10 09:58:49 +0100

    Main file updated.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,27 @@
+package apes;
+
+import javax.swing.JFrame;
+
 /**
+ * This is where it all starts. This creates a basic GUI with a layout
+ * and on it a few components are placed.
  *
+ * @author Johan Andersson (johandy@student.chalmers.se)
  */
-public class Main
+public class Main extends JFrame
 {
-  /**
-   *
-   */
+  public static void main( String[] args )
+  {
+    new Main();
+  }
+
   public Main()
   {
+    setDefaultCloseOperation( EXIT_ON_CLOSE );
 
+    // To come
+
+    pack();
+    setVisible( true );
   }
 }
