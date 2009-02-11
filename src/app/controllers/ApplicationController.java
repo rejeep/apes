942bacadd4d073b43be7f77a62f99fd8190a74fa
Johan Andersson <johan.rejeep@gmail.com>
2009-02-11 15:36:48 +0100

    Added comments to class ApplicationController.

--- src/app/controllers/ApplicationController.java
+++ src/app/controllers/ApplicationController.java
@@ -1,7 +1,11 @@
 package apes;
 
 /**
+ * This class contains stuff common to all controllers. All
+ * controllers, other than {@link ActionController ActionController},
+ * should extend this class.
  *
+ * @author Johan Andersson (johandy@student.chalmers.se)
  */
 public class ApplicationController
 {
