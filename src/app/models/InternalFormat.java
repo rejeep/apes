8f6100c0688890ee07540ab2572cf54837430926
Agentlien <Agentlien@gmail.com>
2009-02-11 12:11:09 +0100

    Added InternalFormat class with basic methods and nested class Channel.

---
+++ src/app/models/InternalFormat.java
@@ -0,0 +1,9 @@
+/**
+ * Describes the audio in a format suitable for internal representation in the program.
+ * @author Daniel Kvick (kvick@student.chalmers.se)
+ *
+ */
+public class InternalFormat 
+{
+	
+}
