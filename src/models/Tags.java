5e91bff2591f56cf8015a654aeab1427a843db7f
unknown <kores.sophie@gmail.com>
2009-02-24 16:18:14 +0100

    implemented tags-class without comments.

--- src/models/Tags.java
+++ src/models/Tags.java
@@ -1,6 +1,31 @@
 package apes.models;
 
-public class Tags
+import java.util.HashMap;
+/**
+ *
+ * @author sophie kores
+ */
+public class Tags extends HashMap<String, String>
 {
+    /**
+     *
+     * @param k
+     * @param v
+     * @return
+     */
+      public String put( String k, String v )
+      {
+            return super.put( k, v );
+      }
+      /**
+       *
+       * @param k
+       * @return the value
+       */
+      public String get( String k )
+      {
+            return super.get( k );
+      }
+
 
 }
