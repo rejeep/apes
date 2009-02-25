af4507b4a0a750f6279c73574d79028335ab28d7
Simon Holm <simon@dhcp-197-65.nomad.chalmers.se>
2009-02-25 15:43:55 +0100

    Added documentation

--- src/lib/FileHandler.java
+++ src/lib/FileHandler.java
@@ -17,7 +17,7 @@ public class FileHandler
    * @param path     The path to the folder with the file to be loaded.
    * @param fileName The name of the file to be loaded.
    * @return Returns a ByteBuffer with all the data from the file.
-   * @throws IOException Throws a IOException if something unexpected happens.
+   * @throws IOException Throws an IOException if something unexpected happens.
    */
   public static ByteBuffer loadFile( String path, String fileName ) throws IOException
   {
@@ -29,11 +29,25 @@ public class FileHandler
     return ByteBuffer.wrap( buffer );
   }
 
+  /**
+   * Loads a file at the root path from the disk and stores the data into a ByteBuffer
+   *
+   * @param fileName The name of the file to be loaded.
+   * @return Returns a ByteBuffer with all the data from the file
+   * @throws IOException Throws an IOException if something unexpected happens.
+   */
   public static ByteBuffer loadFile( String fileName) throws IOException
   {
     return loadFile(".",fileName);
   }
 
+  /**
+   * Saves data to a file.
+   * @param path The path to the folder were the file will be save.
+   * @param fileName The name of the file to be saved.
+   * @param data The data to be saved.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   */
   public static void saveToFile( String path, String fileName, byte[] data ) throws IOException
   {
     File file = new File( path + "/" + fileName );
@@ -42,11 +56,24 @@ public class FileHandler
     oStream.write(data);
   }
 
+  /**
+   * Saves data to a file in the folder ".".
+   * @param fileName The name of the file.
+   * @param data The data to be saved.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   */
   public static void saveToFile( String fileName, byte[] data ) throws IOException
   {
     saveToFile( ".", fileName, data );
   }
 
+  /**
+   * Saves an object to a file.
+   * @param path The path to were the file will be saved.
+   * @param fileName The name of the file.
+   * @param data The object to be saved.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   */
   public static void saveObjectToFile( String path, String fileName, Object data ) throws IOException
   {
     File file = new File( path + "/" + fileName );
@@ -57,12 +84,26 @@ public class FileHandler
     obj_out.writeObject( data );
   }
 
+  /**
+   * Saves an object to a file in the folder ".".
+   * @param fileName The name of the file.
+   * @param data The object to be saved.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   */
   public static void saveObjectToFile( String fileName, Object data ) throws IOException
   {
     saveObjectToFile( ".", fileName, data );
   }
 
 
+  /**
+   * Loads an object from a file.
+   * @param path The path to the folder were to load the file.
+   * @param fileName The name of the file to be loaded.
+   * @return Returns the loaded object.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   * @throws ClassNotFoundException Throws an ClassNotFoundException if something unexpected happens.
+   */
   public static Object loadObjectFile( String path, String fileName ) throws IOException, ClassNotFoundException
   {
     File file = new File( path + "/" + fileName );
@@ -73,6 +114,13 @@ public class FileHandler
     return obj_in.readObject();
   }
 
+  /**
+   * Loads an object form the folder ".".
+   * @param fileName The name of the file to be loaded.
+   * @return Returns the loaded object.
+   * @throws IOException Throws an IOException if something unexpected happens.
+   * @throws ClassNotFoundException Throws an ClassNotFoundException if something unexpected happens.
+   */
   public static Object loadObjectFile( String fileName ) throws IOException, ClassNotFoundException
   {
     return loadObjectFile(".", fileName);
