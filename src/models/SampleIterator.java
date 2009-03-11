5ed458c5ff10f715d198edee4bf39486f7a10413
Agentlien <Agentlien@gmail.com>
2009-03-11 10:38:18 +0100

    Added SampleIterator

---
+++ src/models/SampleIterator.java
@@ -0,0 +1,70 @@
+package models;
+
+import apes.models.Channel;
+import apes.models.Samples;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SampleIterator<Integer> implements Iterator<Integer>
+{
+  
+  /**
+   * Consts a SampleIterator iterating over all samples in the given <code>Channel</code>.
+   * @param c The <code>Channel</code> to iterate over.
+   */
+  public SampleIterator( Channel c )
+  {
+    channel = c;
+    samplesObject = 0;
+    samplesIndex = 0;
+  }
+  
+  /**
+   * Returns false if there are no more samples in the Iterator. Otherwise, returns true.
+   */
+  public boolean hasNext()
+  {
+    return samplesObject < channel.getSamplesSize();
+  }
+  
+  /**
+   * Returns the amplitude of the next sample.
+   */
+  public Integer next()
+  {
+    if( hasNext() )
+    {
+      Integer amplitude = new Integer(channel.getSamples(samplesObject).getSample(samplesIndex));
+      if(++samplesIndex >= channel.getSamples(samplesObject).getSize())
+      {
+        samplesObject++;
+        samplesIndex = 0;
+      }
+      return amplitude;
+    }
+    throw new NoSuchElementException();
+  }
+  
+  /**
+   * Not implemented.
+   */
+  public void remove()
+  {
+    throw new UnsupportedOperationException();
+  }
+  
+  /**
+   * The channel to iterate over.
+   */
+  private Channel channel;
+  
+  /**
+   * Which Samples object is currently being iterated over.
+   */
+  private int samplesObject;
+  
+  /**
+   * Current index within the current samples object. 
+   */
+  private int samplesIndex;
+}
