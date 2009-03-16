8c75d534b17d1e4f72ab3028cd0a826930aae43b
Johan Andersson <johan.rejeep@gmail.com>
2009-03-16 12:42:23 +0000

    Added empty classes for undo/redo and copy/cut/paste actions.

---
+++ src/controllers/undo/CopyAction.java
@@ -0,0 +1,12 @@
+package apes.controllers.undo;
+
+/**
+ * 
+ *
+ * @author Johan Andersson (johandy@student.chalmers.se)
+ * @author Johan Ålander (johan.ahlander@gmail.com)
+ */
+public class CopyAction
+{
+  
+}
---
+++ src/models/undo/CopyEdit.java
@@ -0,0 +1,14 @@
+package apes.models.undo;
+
+import javax.swing.undo.AbstractUndoableEdit;
+
+/**
+ * 
+ *
+ * @author Johan Andersson (johandy@student.chalmers.se)
+ * @author Johan Ålander (johan.ahlander@gmail.com)
+ */
+public class CopyEdit extends AbstractUndoableEdit
+{
+  
+}
