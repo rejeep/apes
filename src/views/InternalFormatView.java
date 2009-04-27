1eefa7e7ce0da3e45822d5a57ff7b6e2cef68e78
Johan Andersson <johan.rejeep@gmail.com>
2009-04-27 10:33:22 +0000

    InternalFormatView has a new job.

--- src/views/InternalFormatView.java
+++ src/views/InternalFormatView.java
@@ -1,67 +1,29 @@
 package apes.views;
 
-import java.awt.Dimension;
-import java.awt.Graphics;
 import javax.swing.JPanel;
 
-import apes.models.Channel;
-import apes.models.InternalFormat;
 import apes.models.Player;
+import apes.models.InternalFormat;
 
-import apes.models.Samples;
+import apes.views.ChannelView;
 
 /**
- * 
+ * Contains one ChannelView per channel in the internal format.
  *
  * @author Johan Andersson (johandy@student.chalmers.se)
  */
 public class InternalFormatView extends JPanel
 {
-  private int width;
-  private int height;
-  
+  /**
+   * Places one ChannelView for each channel on this panel.
+   */
   public InternalFormatView()
   {
-    this.width = 800;
-    this.height = 300;
-    
-    setPreferredSize( new Dimension( width, height ) );
-  }
+    InternalFormat internalFormat = Player.getInstance().getInternalFormat();
 
-  @Override
-  protected void paintComponent( Graphics g )
-  {
-    super.paintComponent( g );
-    
-    Player player = Player.getInstance();
-    InternalFormat internalFormat = player.getInternalFormat();
-    Channel channel = internalFormat.getChannel( 0 );
-    int size = channel.getSamplesSize();
-    Samples samples = null;
-    int min = channel.getMinAmplitude();
-    int max = channel.getMaxAmplitude();
-    long diff = channel.getDiffAmplitude();
-    int prev_y = height / 2;
-      
-    for( int i = 0, j = 0; i < size && j < width; i++, j += 5 )
+    for( int i = 0; i < internalFormat.getNumChannels(); i++ )
     {
-      try
-      {
-        samples = channel.getSamples( i );
-      }
-      catch( Exception e )
-      {
-        e.printStackTrace();
-      }
-      
-      long temp = (long)samples.getAverageAmplitude(10) - min;
-
-      double part = (double)temp / diff;
-      int y = (int)( part * height ) / 2;
-      
-      g.drawLine( j - 1, prev_y, j, y );
-      
-      prev_y = y;
+      add( internalFormat.getChannel( i ) );
     }
   }
 }
