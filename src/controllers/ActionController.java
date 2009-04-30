f6eeb8f1762827ce18cfa2ad37617c0d9dc1650e
Johan Andersson <johan.rejeep@gmail.com>
2009-04-30 17:01:05 +0000

    Comment updates.

--- src/controllers/ActionController.java
+++ src/controllers/ActionController.java
@@ -9,49 +9,61 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 /**
- * <p>This class handles all actions. All controller should extend
- * ApplicationController, who in turn extend from this class. This
- * means that all controllers will handle events the same way.</p>
+ * <p>This class handles all controller actions. All controllers
+ * should extend ApplicationController, that in turn extend this
+ * class. This means that all controllers will handle events the same
+ * way.</p>
+ *
+ * <h4>Usage</h4>
  *
  * <p>If you want to connect an event to a <code>Component</code>, Say
- * for example a JButton, you add a listener as usual:</p>
+ * for example a <code>JButton</code>, you add a listener as
+ * usual:</p>
  *
  * <pre>JButton button = new JButton("Some button");
  *button.addActionListener(controller);</pre>
  *
- * <p>Note that the event should <b>always</b> go to the
+ * <p>Note that the event should <strong>always</strong> go to the
  * controller.</p>
  *
  * <p>After this there's one more thing to do. And that is to give the
- * Component a name with the <code>{@link
- * java.awt.Component#setName(String name) setName}</code> method.</p>
+ * Component a name with the {@link java.awt.Component#setName(String
+ * name) setName} method.</p>
  * <pre>button.setName("action");</pre>
  *
- * <p>The name is the action that is called in the controller that was
- * connected to the event. So for the above example the "action"
- * method will be called when the button is clicked.</p>
+ * <p>The name of the method that will be called in the controller,
+ * that was connected to the component, is the one with the same name
+ * as the component name. This however, is only true, if there's a
+ * method in the controller with that name, taking no arguments. So
+ * for the above example the <code>action</code> method in the
+ * <code>controller</code> controller will be called when
+ * <code>button</code> is clicked.</p>
  *
- * <p>However. If there is no method named action. A method defined in
- * {@link ApplicationController#methodMissing ApplicationController}
- * called methodMissing is called. This is handy for dynamic action
- * calling. So in your controller, you can override that method to get
- * some behavior.</p>
+ * <p>However. If there is no method named action in the controller. A
+ * method defined in {@link ApplicationController#methodMissing
+ * ApplicationController} called <code>methodMissing</code> is
+ * called. This is handy for dynamic action calling. So in your
+ * controller, you can override this method to get some other
+ * behavior.</p>
  *
- * <p>Two variables are by default accessible in the controllers.
+ * <p>Two variables are by default available in the controllers.
  * <ul>
- *   <li>event - Is the event that occurred.</li>
- *   <li>name - Is the name of the component.</li>
+ *   <li>event - Is the event that was fired.</li>
+ *   <li>name - Is the name of the component that the event was fired on.</li>
  * </ul>
  * </p>
  *
- * <p>If the above do not work, start by reading the comments in
+ * <h4>Troubleshooting</h4>
+ *
+ * <p>If the above does not work. Start by reading the comments in
  * {@link ApplicationController ApplicationController}.</p>
  *
  * <p>If you done that and it still doesn't work, make sure that this
  * class implements the listener class you want to use. Say for
  * example that you want to add a <code>ComponentListener</code> to
- * your controller. Then this class must implement ComponentListener
- * and have all methods from that interface. In this case:
+ * your controller. Then this class must implement
+ * <code>ComponentListener</code> and have all methods from that
+ * interface. In this case:</p>
  * <ul>
  *   <li>void componentHidden(ComponentEvent e)</li>
  *   <li>void componentMoved(ComponentEvent e)</li>
@@ -59,13 +71,14 @@ import javax.swing.event.ChangeListener;
  *   <li>void componentShown(ComponentEvent e)</li>
  * </ul>
  *
- * And in each of these methods you should first set the event
+ * <p>And in each of these methods you should first set the event
  * instance variable to the event that is passed to the method. And
  * then call {@link ActionController#callActionByName callActionByName}.</p>
  *
- * One more thing to check. Is your controller initialized? Because if
- * you send null to addActionListener, you don't get any error.
- * 
+ * <p>One more thing to check is if your controller is created or not.
+ * Because if you send null to <code>addActionListener</code>, you don't get any
+ * errors.</p>
+ *
  * @author Johan Andersson (johandy@student.chalmers.se)
  */
 public class ActionController implements ActionListener, ChangeListener
