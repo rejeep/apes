4186b11d6e5531fec1094717fa704c026d018463
Johan Åhlander <johan.ahlander@gmail.com>
2009-05-03 16:05:58 +0200

    Added PluginController.

---
+++ src/controllers/PluginController.java
@@ -0,0 +1,51 @@
+package apes.controllers;
+
+import apes.views.PluginView;
+import apes.lib.PluginHandler;
+import java.util.Map;
+import java.util.ArrayList;
+import javax.swing.*;
+
+public class PluginController extends ApplicationController
+{
+  PluginView pluginView;
+  PluginHandler pluginHandler;
+
+  public PluginController()
+  {
+    pluginHandler = new PluginHandler();
+		pluginView = new PluginView(pluginHandler, this);
+  }
+  
+  public void plugin()
+  {
+    pluginView.create();
+  }
+  
+	public void apply()
+	{
+		Map<String, JCheckBox> choices = pluginView.getChoices();
+		ArrayList<String> names = pluginHandler.getPluginNames();
+		
+		for(String name : names)
+		{
+			if( !(pluginHandler.isLoaded(name) == choices.get(name).isSelected()) )
+			{
+				if(pluginHandler.isLoaded(name))
+				{
+					pluginHandler.unloadPlugin(name);
+				}
+				else
+				{
+					pluginHandler.loadPlugin(name);
+				}
+			}
+		}
+	}
+	
+	public void close()
+  {
+    pluginView.setVisible(false);
+    pluginView.dispose();
+  }
+}
