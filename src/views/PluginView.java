3c2c4b66e5fd88a21567715231929d39c509d74f
Johan Åhlander <johan.ahlander@gmail.com>
2009-05-03 20:40:18 +0200

    New layout.

--- src/views/PluginView.java
+++ src/views/PluginView.java
@@ -1,27 +1,59 @@
 package apes.views;
 
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
-import javax.swing.JLabel.*;
-import apes.interfaces.AudioFormatPlugin;
-import apes.interfaces.Plugin;
-import apes.interfaces.TransformPlugin;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JFrame;
+import javax.swing.JCheckBox;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.BorderFactory;
 import java.util.ArrayList;
-import apes.lib.PluginHandler;
-import apes.controllers.PluginController;
 import java.util.HashMap;
 import java.util.Map;
-import apes.lib.Language;
 
+import apes.lib.Language;
+import apes.lib.PluginHandler;
+import apes.controllers.PluginController;
+
+/**
+ * Graphical view for the plugins.
+ *
+ * @author Johan Åhlander (johan.ahlander@gmail.com)
+ */
 public class PluginView extends JFrame
 {
+  /**
+   *  The plugin controller
+   */
   private PluginController pluginController;
+  
+  /**
+   * The plugin handler
+   */
   private PluginHandler pluginHandler;
+  
+  /**
+   * Mapping to fetch selected values from checkboxes.
+   */
   private Map<String, JCheckBox> choices;
+  
+  /**
+   * The language object.
+   */
   private Language language;
   
+  /**
+   * Creates a new <code>PluginView/code> instance.
+   *
+   * @param pH plugin handler.
+   * @param Pc plugin controller
+   */
   public PluginView(PluginHandler pH, PluginController pC)
   {
     pluginController = pC;
@@ -30,31 +62,74 @@ public class PluginView extends JFrame
     language = Language.getInstance();
   }
   
+  /**
+   * Creates the frame.
+   */
   public void create()
   {      
     setLayout(new BorderLayout());
     add(createPluginPanel(), BorderLayout.NORTH);
     add(createButtonPanel(), BorderLayout.SOUTH);
+    setDefaultCloseOperation( EXIT_ON_CLOSE );
     pack();
+    setLocationRelativeTo( null );
     setVisible(true);
   }
   
+  /**
+   * Returns the top panel with selectable plugins.
+   * 
+   * @return The top panel.
+   */
   public JPanel createPluginPanel()
   {
     JPanel panel = new JPanel();
+    panel.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
+    panel.setLayout( new BorderLayout() );
+    
+    JPanel top = new JPanel();
+    JLabel header = new ApesLabel( "plugins.header" );
+    header.setFont( new Font( "verdana", 1, 20 ) );
+    top.add(header);
+    panel.add(top, BorderLayout.NORTH);
+    
+    JPanel bottom = new JPanel();
+    GridBagConstraints c = new GridBagConstraints();
+    GridBagLayout gridbag = new GridBagLayout();
+    
+    bottom.setLayout(gridbag);
+    bottom.setBorder( BorderFactory.createEmptyBorder( 10, 10, 10, 10 ) );
+    
     ArrayList<String> names = pluginHandler.getPluginNames();
-    panel.setLayout(new GridLayout(names.size(), 2));
+    
     for(int i=0; i<names.size(); i++)
     {
       JCheckBox pBox = new JCheckBox(names.get(i), pluginHandler.isLoaded(names.get(i)));
       JTextArea pText = new JTextArea(pluginHandler.getDescription(names.get(i)));
-      panel.add(pBox, BorderLayout.WEST);
-      panel.add(pText, BorderLayout.EAST);
+      
+      c.gridx = 0;
+      c.gridy = i+1;
+      c.gridwidth = 1;;
+      c.ipadx = 10;
+      c.fill = GridBagConstraints.HORIZONTAL; 
+      gridbag.setConstraints(pBox, c);
+      bottom.add(pBox);
+      c.gridx = 1;
+      gridbag.setConstraints(pText, c);
+      bottom.add(pText);
       choices.put(names.get(i), pBox);
     }
+    
+    panel.add(bottom, BorderLayout.SOUTH);
+    
     return panel;
   }
   
+  /**
+   * Returns the bottom panel with buttons.
+   * 
+   * @return The bottom panel.
+   */
   public JPanel createButtonPanel()
   {
     JPanel panel = new JPanel();
@@ -72,6 +147,11 @@ public class PluginView extends JFrame
     return panel;
   }
   
+  /**
+   * Returns mapping with plugin names and checkboxes.
+   * 
+   * @return Selected options.
+   */
   public Map<String, JCheckBox> getChoices()
   {
     return choices;
