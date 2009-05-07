2f9de7b196e22e65470617e88a6dabeb88fcd8bb
Johan Andersson <johan.rejeep@gmail.com>
2009-05-07 12:35:48 +0000

    Added new class SampleHelper that is meant to be used for sample calculations.

---
+++ src/lib/SampleHelper.java
@@ -0,0 +1,47 @@
+/**
+ *
+ *
+ * @author Johan Andersson (johandy@student.chalmers.se)
+ */
+public class SampleHelper
+{
+  /**
+   *
+   */
+  private int sampleRate;
+
+  public SampleHelper( int sampleRate )
+  {
+    this.sampleRate = sampleRate;
+  }
+
+  public int millisecondsToSamples( int milliseconds )
+  {
+
+  }
+
+  public int secondsToSamples( int seconds )
+  {
+
+  }
+
+  public int minutesToSamples( int minutes )
+  {
+
+  }
+
+  public int samplesToMilliseconds( samples )
+  {
+
+  }
+
+  public int samplesToseconds( samples )
+  {
+
+  }
+
+  public int samplesToMinutes( samples )
+  {
+
+  }
+}
