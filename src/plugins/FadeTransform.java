0e97967008c99e53c95832833d13222ad5483512
Johan Åhlander <johan.ahlander@gmail.com>
2009-05-09 19:00:40 +0200

    Add choice frame

--- src/plugins/FadeTransform.java
+++ src/plugins/FadeTransform.java
@@ -1,15 +1,24 @@
 package apes.plugins;
 
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
 import java.util.Map;
 import java.util.HashMap;
+import javax.swing.JFrame;
+import javax.swing.JButton;
 
 import apes.interfaces.TransformPlugin;
 import apes.models.Samples;
 import apes.models.InternalFormat;
 
-public class FadeTransform implements TransformPlugin
+public class FadeTransform implements TransformPlugin, ActionListener
 {
+  JFrame frame;
+  InternalFormat internalFormat;
+  Point selection;
+  
   public String getName()
   {
     return "Fade";
@@ -22,9 +31,26 @@ public class FadeTransform implements TransformPlugin
     map.put("sv", "-");
     return map;
   }
+  
+  public void showFrame()
+  {
+    frame = new JFrame();
+    frame.setLayout( new BorderLayout() );
+    JButton inButton = new JButton("Fade in");
+    inButton.addActionListener(this);
+    frame.add(inButton, BorderLayout.NORTH);
+    JButton outButton = new JButton("Fade out");
+    outButton.addActionListener(this);
+    frame.add(outButton, BorderLayout.SOUTH);
+    frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation( frame.DISPOSE_ON_CLOSE );
+    frame.pack();
+    frame.setVisible(true);
+  }
 
   public void apply( InternalFormat internalFormat, Point selection )
   {
+    showFrame();
     int interval = 100;
     int diff = selection.y - selection.x;
     float scale;
@@ -35,6 +61,18 @@ public class FadeTransform implements TransformPlugin
       scale = 0.05f;
       System.out.println("Setting for channel " + i+1);
       internalFormat.getChannel(i).scaleSamples(selection.x, selection.y, scale);
+    }
+  }
+  
+  public void actionPerformed(ActionEvent ae)
+  {
+    String action = ae.getActionCommand();
+    System.out.println(action);
+    if(action.equals("Fade in"))
+    {
+    }
+    else if(action.equals("Fade out"))
+    {
     }
   }
 }
