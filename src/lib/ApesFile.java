d99851cfda0cceeee1f49abcb63b0704f7a979b0
Johan Andersson <johan.rejeep@gmail.com>
2009-05-12 13:55:59 +0000

    Added method to ApesFile that can read in a file.

--- src/lib/ApesFile.java
+++ src/lib/ApesFile.java
@@ -3,6 +3,9 @@ package apes.lib;
 import java.io.File;
 import apes.interfaces.AudioFormatPlugin;
 import apes.models.InternalFormat;
+import javax.swing.JFileChooser;
+import apes.exceptions.UnidentifiedLanguageException;
+import apes.views.ApesError;
 
 /**
  * Create a new object of this class with a file on the system. Then
@@ -16,8 +19,8 @@ public class ApesFile
    * The file.
    */
   private File file;
-  
-  
+
+
   /**
    * Creates a new <code>ApesFile</code> instance.
    *
@@ -27,7 +30,7 @@ public class ApesFile
   {
     this( new File( System.getProperty( "user.dir" ) + File.separator + fileName ) );
   }
-  
+
   /**
    * Creates a new <code>ApesFile</code> instance.
    *
@@ -47,7 +50,7 @@ public class ApesFile
   {
     return file.getName();
   }
-  
+
   /**
    * Creates new internal format from the file.
    *
@@ -58,7 +61,41 @@ public class ApesFile
   public InternalFormat getInternalFormat() throws Exception
   {
     AudioFormatPlugin audioFile = new ApesFormat( file ).getAudioFile();
-    
+
     return audioFile.importFile( file.getParent(), getName() );
+  }
+  
+  /**
+   * Opens a file with a graphical interface.
+   *
+   * @return An <code>ApesFile</code> for the selected file, or null
+   * of no file was selected.
+   */
+  public static ApesFile open()
+  {
+    try
+    {
+      // Chose file.
+      JFileChooser fileChooser = new JFileChooser();
+      fileChooser.setCurrentDirectory( new File( "." ) );
+      int returnVal = fileChooser.showOpenDialog( null );
+
+      if( returnVal == JFileChooser.APPROVE_OPTION )
+      {
+        File file = fileChooser.getSelectedFile();
+
+        return new ApesFile( file );
+      }
+    }
+    catch( UnidentifiedLanguageException e )
+    {
+      ApesError.unsupportedFormat();
+    }
+    catch( Exception e )
+    {
+      e.printStackTrace();
+    }
+
+    return null;
   }
 }
